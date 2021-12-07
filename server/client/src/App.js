cd51dc4622c5faef94f087f7066e2cf8111f9f36
gabejose <g.jose10xx@gmail.com>
2021-12-07 11:05:16 +0000

    QSPC-27 #comment completed navbar with logo test

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,5 +1,19 @@
+import NavBar from './components/navbar';
+import Home from './homePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>);
 }
 
 export default App;
---
+++ server/client/src/components/navbar.jsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+
+const navbar = () => {
+    return (
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="http://www.staggeringbeauty.com/">Logo goes here</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/OpeningTimes">Opening Times</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/NewReleases">New Releases</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/FilmClassification">Film Classification</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/ScreenListings">Screen Listings</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/About">About</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/Contact"></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default navbar;
