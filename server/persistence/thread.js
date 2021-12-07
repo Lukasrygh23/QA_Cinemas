386bcf36f0a3f76b2aa786814aab39ff54dda0e1
LukasR23 <Lukas@jazzcatz.com>
2021-12-07 11:29:17 +0000

    QSPC-61 #comment Attempt to combine threads and comments.

--- server/persistence/thread.js
+++ server/persistence/thread.js
@@ -4,11 +4,19 @@ const mongoose = require("mongoose");
 //Create Schema and Model
 const { Schema, model } = mongoose;
 
+const commentSchema = new Schema({
+  username: String,
+  text: String
+  //This is a very imaginiative Schema
+});
+
 const threadSchema = new Schema({
   id: number,
   userName: String,
   rating: { type: Number, min: 0, max: 10 },
   reviewBody: String,
+  comments: [commentSchema],
+  comment: commentSchema
 });
 
 const Thread = model("Thread", threadSchema);
