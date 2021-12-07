69cb96c5a937a366d0309e7f49fd3f33ecab2ff7
samkazshel <samsheldon779@gmail.com>
2021-12-07 10:35:29 +0000

    QSPC-61 #comment created basic review schema

--- server/persistence/review.js
+++ server/persistence/review.js
@@ -1,1 +1,16 @@
 //review
+//importing mongoose
+const mongoose = require("mongoose");
+//Create Schema and Model
+const { Schema, model } = mongoose;
+
+const reviewSchema = new Schema({
+  id: number,
+  userName: String,
+  rating: { type: Number, min: 0, max: 10 },
+  reviewBody: String,
+});
+
+const Review = model("Review", reviewSchema);
+
+module.exports = { Review: Review };
