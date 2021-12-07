1fd861b5c1476715ae60293ffa28b17e857bf589
gabejose <g.jose10xx@gmail.com>
2021-12-07 11:12:02 +0000

    QSPC-27 #comment added a target=blank to logo test

--- server/client/src/components/navbar.jsx
+++ server/client/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
     return (
         <nav class="navbar navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="http://www.staggeringbeauty.com/">Logo goes here</a>
+                <a class="navbar-brand" href="http://www.staggeringbeauty.com/" target="_blank">Logo goes here</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
