3c5c12ee78320d3a89aeef30e0892142442025e4
samkazshel <samsheldon779@gmail.com>
2021-12-07 10:23:26 +0000

    QSPC-63 #comment created basic screening schema

--- server/persistence/screening.js
+++ server/persistence/screening.js
@@ -1,1 +1,17 @@
 //screening
+//importing mongoose
+const mongoose = require("mongoose");
+//Create Schema and Model
+const { Schema, model } = mongoose;
+
+const screeningSchema = new Schema({
+  id: Number,
+  screenNumber: Number,
+  screenType: { enum: ["Regular", "Deluxe"] },
+  date: Date,
+  time: String,
+});
+
+const Screening = model("Screening", screeningSchema);
+
+module.exports = { Screening: Screening };
