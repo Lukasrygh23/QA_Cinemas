24049df78592d4a6aa044362aa427ae112053100
gabejose <g.jose10xx@gmail.com>
2021-12-08 11:02:39 +0000

    QSPC-27 #comment added dropdowns and search bar to navbar

--- server/client/src/components/navbar.jsx
+++ server/client/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
     return (
         <nav class="navbar navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="http://www.staggeringbeauty.com/" target="_blank">Logo goes here</a>
+                <a class="navbar-brand" href="http://www.staggeringbeauty.com/" target="_blank">Logo</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -14,11 +14,27 @@ const navbar = () => {
                             <Link to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <Link to="/movie/no_time_to_die">Opening Times</Link>
+                            <Link to="/OpeningTimes">Opening Times</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="/NewReleases">New Releases</Link>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Current Releases
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li class="nav-item"><Link class="dropdown-item" to="/movie/no_time_to_die">No Time To Die</Link></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
                         </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Upcoming Releases
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li class="nav-item"><Link to="/NewReleases">View All</Link></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
+                        </li>
+
                         <li class="nav-item">
                             <Link to="/FilmClassification">Film Classification</Link>
                         </li>
@@ -32,6 +48,10 @@ const navbar = () => {
                             <Link to="/Contact"></Link>
                         </li>
                     </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" name="search" aria-label="Search"></input>
+                        <button class="button-62 mb-2" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </nav>
