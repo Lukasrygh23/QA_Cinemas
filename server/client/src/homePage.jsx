a08c541040e0263b964426349af88130d468dff9
solomonboundy1 <solomonboundy2017@gmail.com>
2021-12-08 14:06:25 +0000

    QSP-27 #comment cardlist updated

--- server/client/src/homePage.jsx
+++ server/client/src/homePage.jsx
@@ -1,9 +1,13 @@
 import CarouselGallery from './components/CarouselGallery';
+import CardListUpcomingMovies from './components/CardListUpcomingMovies';
+import Footer from './components/Footer';
 
 const homePage = () => {
     return (
         <div>
             <CarouselGallery />
+            <CardListUpcomingMovies />
+            <Footer/>
         </div>
     );
 }
