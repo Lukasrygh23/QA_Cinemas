43b3d10e636145dca674b55478abfe37404f0589
solomonboundy1 <solomonboundy2017@gmail.com>
2021-12-09 11:36:59 +0000

    QSPC-31 #comment film classifications page first draft complete

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,6 +5,7 @@ import BookingNoTimeToDiePage from './BookingNoTimeToDiePage';
 import OpeningTimes from './openingTimes';
 import ScreenListings from './screenListingsPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import FilmClassification from './components/FilmClassification';
 
 function App() {
   return (
@@ -25,6 +26,9 @@ function App() {
         </Route>
         <Route path="/ScreenListings">
           <ScreenListings />
+        </Route>
+        <Route path="/FilmClassification">
+          <FilmClassification/>
         </Route>
       </Switch>
     </Router>
