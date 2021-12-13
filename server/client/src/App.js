516fa1103bed777e04ab5b6ad04b583a6fe65933
Man Chung Daniel Li <danli@Mans-MacBook-Pro-2.local>
2021-12-13 18:05:03 +0000

    QSPC-5 #comment Temporarily commented out contactPage component as it is causing client side errors when running npm run dev, propose fixing after merging to dev.

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,7 +5,7 @@ import BookingNoTimeToDiePage from './BookingNoTimeToDiePage';
 import OpeningTimes from './openingTimes';
 import ScreenListings from './screenListingsPage';
 import AboutPage from './aboutPage';
-import ContactPage from './ContactPage';
+// import ContactPage from './ContactPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import FilmClassificationPage from './FilmClassificationPage';
 import Discussion from "./components/Discussion";
@@ -97,9 +97,9 @@ function App() {
         <Route path="/FilmClassification">
           <FilmClassificationPage />
         </Route>
-        <Route path="/Contact">
+        {/* <Route path="/Contact">
           <ContactPage />
-        </Route>
+        </Route> */}
         <Route path="/Discussion">
           <Discussion />
         </Route>
