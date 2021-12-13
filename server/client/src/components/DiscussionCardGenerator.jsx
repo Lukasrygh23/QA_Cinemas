d1af7d9e1931f45a8b6f8c4e41892e1f5cce5d3a
samkazshel <samsheldon779@gmail.com>
2021-12-13 12:52:23 +0000

    QSPC-20 #comment form working, some formatting done

--- server/client/src/components/DiscussionCardGenerator.jsx
+++ server/client/src/components/DiscussionCardGenerator.jsx
@@ -10,25 +10,36 @@ const DicussionCardGenerator = (props) => {
           <div className="row  mt-5 mb-3">
             <div className="col-sm-8">
               <h2 className="text-light mt-3 mb-3">{Thread.subject}</h2>
-              <p className="text-light">{Thread.userName} </p>
-              <p className="text-light">{Thread.rating} </p>
+              <p className="text-light">
+                <b>Username:</b> {Thread.userName}{" "}
+              </p>
+              <p className="text-light">
+                <b>Rating: </b>
+                {Thread.rating}{" "}
+              </p>
               <p className="text-light">{Thread.reviewBody} </p>
               <div>
                 {Thread.comments.map((comment) => (
-                  <div>
-                    <h6 className="text-light">{comment.username}</h6>
-                    <p className="text-light">{comment.text}</p>
+                  <div className="container">
+                    <div class="col-sm-4">
+                      <h6 className="text-light">
+                        <b>Username:</b> {comment.username}
+                      </h6>
+                      <p className="text-light">
+                        {" "}
+                        <b>Comment: </b>
+                        {comment.text}
+                      </p>
+                    </div>
                   </div>
                 ))}
               </div>
-              <br />
               <div></div>
             </div>
           </div>
           <hr className="hrTag" />
         </div>
       </div>
-      <br />
     </div>
   );
 };
